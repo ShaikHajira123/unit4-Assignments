ae21ef5fa4c432456ddeea16c27896c926f24e6f
ShaikHajira123 <97529565+ShaikHajira123@users.noreply.github.com>
2022-03-02 21:37:09 +0530

    calculator done
    day1 assignment done

---
+++ main.js
@@ -0,0 +1,13 @@
+let a=50
+let b=40
+const add = require("./add")
+const substract=require("./substract")
+const multiply=require("./multiply")
+const divide=require("./divide")
+
+    console.log(`The addition of a&b is ${add(a,b)}`)
+    console.log(`The substraction of a&b is ${substract(a,b)}`)
+    console.log(`The multiplication of a&b is ${multiply(a,b)}`)
+    console.log(`The division of a&b is ${divide(a,b)}`)
+
+
