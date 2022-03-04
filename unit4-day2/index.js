362a1f22e4dadcf2c4cdd1bd4eef0909472d60d3
ShaikHajira123 <97529565+ShaikHajira123@users.noreply.github.com>
2022-03-04 21:43:03 +0530

    day2 done
    day2 done

--- unit4-day2/index.js
+++ unit4-day2/index.js
@@ -1,10 +1,10 @@
 const express=require("express")
-const add=express()
-add.get("/home",function(req,res){
+const app=express()
+app.get("/home",function(req,res){
     return res.send("hello")
 })
 
-add.get("/books",(req,res)=>{
+app.get("/books",(req,res)=>{
    let books={ firstBook:"english",
                 secondBook:"Maths",
                 thirdBook:"Physics",
@@ -12,6 +12,6 @@ add.get("/books",(req,res)=>{
       return res.send(books)
 })
 
-add.listen(2000,()=>{
+app.listen(2000,()=>{
     console.log("haj")
 })
