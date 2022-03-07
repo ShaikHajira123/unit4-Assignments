eae9bdd50439a51f66fd842b9976287c913f942b
ShaikHajira123 <97529565+ShaikHajira123@users.noreply.github.com>
2022-03-07 19:56:34 +0530

    Update index.js

--- unit4-C1/index.js
+++ unit4-C1/index.js
@@ -1,17 +1,51 @@
 
+// const express=require("express")
+// const app=express()
+// app.get("/home",function(req,res){
+//     return res.send("hello")
+// })
+
+// app.get("/books",(req,res)=>{
+//    let books={ firstBook:"english",
+//                 secondBook:"Maths",
+//                 thirdBook:"Physics",
+//                 fourthBook:"science"}
+//       return res.send(books)
+// })
+// app.listen(2000,(req,res)=>{
+//     console.log("listening on port 2000")
+// })
+
 const express=require("express")
 const app=express()
-app.get("/home",function(req,res){
-    return res.send("hello")
-})
+
+app.use(logger)
 
 app.get("/books",(req,res)=>{
-   let books={ firstBook:"english",
-                secondBook:"Maths",
-                thirdBook:"Physics",
-                fourthBook:"science"}
-      return res.send(books)
+    return res.send({route:"/books",role:req.role})
+ })
+
+app.get("/libraries",logger,checkPermission("librarian"),(req,res)=>{
+    return res.send({route:"/libraries",permission:req.name})
 })
-app.listen(2000,(req,res)=>{
-    console.log("listening on port 2000")
+app.get("/authors",logger,checkPermission("author"),(req,res)=>{
+    return res.send({route:"/authors",permission:req.name})
+})
+function checkPermission(role){
+    return function logger(req,res,next){
+        if(role==="author" || role==="librarian"){
+            req.name=true
+      return next()
+       
+        }
+        }
+    }
+
+function logger(req,res,next){
+   console.log(req.path)
+   return next()
+}
+
+app.listen(4000,()=>{
+    console.log("listening on port 4000")
 })
